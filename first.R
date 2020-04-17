a1dc200f9404a96cab46f2f8d47ce83ae2387783
danfelker <dpfelker@salud.unm.edu>
2020-04-17 11:36:37 -0600

    Added first.R script. This script is the starting point for learning R.

---
+++ first.R
@@ -0,0 +1,18 @@
+## First R Script
+## In this R script you will learn general syntax and how to execute from a script file
+## first things first, any line that starts with a # is ignored by R and is for
+## humans reading the script only; i.e. a comment. So pay special attention
+## to this text as it has instructions and information about the script itself
+## to help you understand what is going on.
+## Ok, lets jump into learning R and RStudio
+## To start with, lets execute a single line of code. In Rstudio if you have a script
+## open (like this one) you can put the cursor on a line you want to execute and hit
+## <ctrl> <enter> together (for macs use command-enter)
+## Try it now on the line below
+
+print("Hello World!")
+
+### after you hit enter you should see in the window below 
+##  print("Hello World!")
+## [1] "Hello World!"
+## > 
